148c1ed464c805cc1970c1dd46b52bb9b0fa364d
Baha El Kassaby <baha.elkassaby@gmail.com>
2016-12-19 13:48:24 +0000

    Update php server

--- org.dawnsci.usagedata.server/download.php
+++ org.dawnsci.usagedata.server/download.php
@@ -7,55 +7,10 @@ $server = mysql_connect ( $host, $user, $password );
 $connection = mysql_select_db ( $database, $server );
 
 // type of query to run : can be "modules", "views", "perspectives", "java", "geo", "os"
-$type = $_GET ["type"];
-switch ($type) {
-	case "modules" :
-		$myquery = "SELECT date_format(time, '%b %Y') as date, count(*) as total_logins, count(distinct(user_id)) as unique_logins
+$myquery = "SELECT date_format(time, '%b %Y') as date, count(*) as total_logins, count(distinct(user_id)) as unique_logins
 FROM dawnstats.moduleload_record
 group by date
 order by id;";
-		break;
-	case "views" :
-		$myquery = "SELECT description, count(*)
-as number
-FROM dawnstats.usagedata_record 
-where kind='view' 
-group by description
-order by description;";
-		break;
-	case "perspectives" :
-		$myquery = "SELECT description, count(*)
-as number
-FROM dawnstats.usagedata_record 
-where kind='perspective' 
-group by description
-order by description;";
-		break;
-	case "java" :
-		$myquery = "SELECT description as 'key', count(*) as y 
-FROM dawnstats.usagedata_record
-where kind='sysinfo'
-and what='java.version'
-group by description
-order by description;";
-		break;
-	case "geo" :
-		$myquery = "SELECT description as 'key', count(*) as y 
-FROM dawnstats.usagedata_record
-where kind='sysinfo'
-and what='locale'
-group by description
-order by description;";
-		break;
-	case "os" :
-		$myquery = "SELECT description as 'key', count(*) as y 
-FROM dawnstats.usagedata_record
-where kind='sysinfo'
-and what='os'
-group by description
-order by description;";
-		break;
-}
 
 $query = mysql_query ( $myquery );
 
--- org.dawnsci.usagedata.server/setup/create_tables.sql
+++ org.dawnsci.usagedata.server/setup/create_tables.sql
@@ -1,46 +1,1 @@
-# mysql -u root -p < create_tables.sql
-
-create database if not exists udc;
-use udc;
-
-#create user 'udc_user'@'localhost' identified by 'udc_password';
-#grant ALL on *.* to 'udc_user'@'localhost';
-
-# geoip table contains the mappings from ip address to country code (ccode).
-drop table if exists geoip;
-create table geoip (start int unsigned, end int unsigned, ccode char(2));
-
-# usagedata_profile table contains an entry for each userid/workstationid pair.
-drop table if exists usagedata_profile;
-create table usagedata_profile (
-	id int unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY, 
-	userId char(40) NOT NULL,
-	workspaceId char(40) NOT NULL
-) Engine=MyISAM;
-
-# usagedata_upload table contains an entry for each upload event.
-drop table if exists usagedata_upload;
-create table usagedata_upload (
-	id int unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	profileId int unsigned NOT NULL,
-	ccode char(2) NULL,
-	time timestamp DEFAULT CURRENT_TIMESTAMP
-) Engine=MyISAM;
-CREATE INDEX IDX_profileId ON usagedata_upload(profileId);
-CREATE INDEX IDX_ccode ON usagedata_upload(ccode);
-
-#usagedata_record table contains a record for each record of usage data.
-drop table if exists usagedata_record;
-create table usagedata_record (
-	id int unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	uploadId int unsigned NOT NULL,
-	what varchar(256),
-	kind varchar(256),
-	bundleId varchar(256),
-	bundleVersion varchar(256),
-	description varchar(256),
-	time bigint unsigned NOT NULL
-)  Engine=MyISAM;
-CREATE INDEX IDX_uploadId ON usagedata_record(uploadId);
-CREATE INDEX IDX_kind_bundleId ON usagedata_record(kind,bundleId);
-CREATE INDEX IDX_bundleId ON usagedata_record(bundleId);
+CREATE TABLE `moduleload_record` (   `id` int(11) NOT NULL,   `time` datetime DEFAULT NULL,   `user_id` varchar(255) DEFAULT NULL,   `workstation` varchar(255) DEFAULT NULL,   `module_load` varchar(255) DEFAULT NULL,   `release_version` varchar(255) DEFAULT NULL,   PRIMARY KEY (`id`) ) ENGINE=InnoDB DEFAULT CHARSET=latin1
